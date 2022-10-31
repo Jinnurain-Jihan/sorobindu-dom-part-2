8744dcf3ba0f9f0c248a3748ab5ba2c964af7c77
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-31 20:11:18 +0600

    DOM part two

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(document.doctype.name);
+
+console.log(document.getElementById("box"));
+console.log(document.getElementsByClassName("fox"));
+console.log(document.getElementsByTagName("div"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.querySelector("h1"));
+console.log(document.querySelectorAll(".fox"));
+console.log(document.querySelector("#box"));
+console.log(document.querySelector("#box")); 
